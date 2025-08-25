28c868db126f918937edc37c95a83336173b061d
Ariane Rossetti <108526841+ArianeRossetti@users.noreply.github.com>
2025-08-25 16:51:32 +0200

    Modulo_3

--- 3_AVANCANDO_EM_REACT/avancando/src/App.js
+++ 3_AVANCANDO_EM_REACT/avancando/src/App.js
@@ -7,10 +7,31 @@ import ListRender from "./components/ListRender";
 import ManageData from "./components/ManageData";
 import ShowUserName from "./components/ShowUserName";
 import CarDetails from "./components/CarDetails";
+import Fragment from "./components/Fragment";
+import Conteiner from "./components/Conteiner";
+import ExecuteFunction from "./components/ExecuteFunction";
+import Message from "./components/Message";
+import ChangeMessageState from "./components/ChangeMessageState";
 
 function App() {
-  const name = "Joaquim";
+  //const name = "Joaquim";
   const [userName] = useState("Maria");
+
+  const cars = [
+    { id: 1, brand: "Ferrari", color: "Amarela", newCar: true, km: 0 },
+    { id: 2, brand: "KIA", color: "Branco", newCar: false, km: 34343 },
+    { id: 3, brand: "Renault", color: "Azul", newCar: false, km: 7234 },
+  ];
+
+  function showMessage() {
+    console.log("Evento do componente pai");
+  }
+
+  const [message, setMessage] = useState("");
+
+  const handleMessage = (msg) => {
+    setMessage(msg);
+  };
 
   return (
     <div className="App">
@@ -35,6 +56,30 @@ function App() {
       {/* reaproveitando */}
       <CarDetails brand={"Ford"} color="Vermelha" km={0} newCar={true} />
       <CarDetails brand={"Fiat"} color="Branco" km={4500} newCar={false} />
+      {/* loop em array de objetos */}
+      {cars.map((car) => (
+        <CarDetails
+          key={car.id}
+          brand={car.brand}
+          color={car.color}
+          km={car.km}
+          newCar={car.newCar}
+        />
+      ))}
+      {/* fragment */}
+      <Fragment propFragment="teste" />
+      {/* children */}
+      <Conteiner myValue="testing">
+        <h5>E este é o conteúdo</h5>
+      </Conteiner>
+      <Conteiner myValue="testing">
+        <h5>Testando o container</h5>
+      </Conteiner>
+      {/* executar funcao */}
+      <ExecuteFunction myFunction={showMessage} />
+      {/* state lift */}
+      <Message msg={message} />
+      <ChangeMessageState handleMessage={handleMessage} />
     </div>
   );
 }
---
+++ 3_AVANCANDO_EM_REACT/avancando/src/components/Conteiner.js
@@ -0,0 +1,11 @@
+const Conteiner = ({ children, myValue }) => {
+  return (
+    <div>
+      <h2>Este é o título do container</h2>
+      {children}
+      <p>O valor é: {myValue}</p>
+    </div>
+  );
+};
+
+export default Conteiner;
